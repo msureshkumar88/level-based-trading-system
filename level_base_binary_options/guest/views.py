8f3fef973b6f0d73734d0b5a2e3bf78a97079677
suresh <suresh12_dmm@yahoo.com>
2020-03-23 17:00:20 +0000

    add test template file

--- level_base_binary_options/guest/views.py
+++ level_base_binary_options/guest/views.py
@@ -6,7 +6,7 @@ from django.http import HttpResponse
 
 def home(request):
 
-    return HttpResponse("Hello world")
+    return render(request, 'home.html')
 
 def login(request):
     return HttpResponse("login")
