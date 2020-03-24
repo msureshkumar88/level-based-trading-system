97eee175c94c10cf8dfc9c1498ca518542ad4211
suresh <suresh12_dmm@yahoo.com>
2020-03-24 13:51:54 +0000

    design register form

--- level_base_binary_options/guest/views.py
+++ level_base_binary_options/guest/views.py
@@ -12,7 +12,7 @@ def home(request):
 def login(request):
 
     if request.method == "POST":
-        
+
         email = request.POST['email']
         password = request.POST['password']
         print(email,password)
@@ -20,4 +20,17 @@ def login(request):
     return render(request, 'login.html')
 
 def register(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        repassword = request.POST['repassword']
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        mobile = request.POST['mobile']
+        address = request.POST['address']
+        country = request.POST['country']
+        currency = request.POST['currency']
+        virtual_currency = request.POST['virtual_currency']
+        print(username)
     return render(request, 'register.html')
