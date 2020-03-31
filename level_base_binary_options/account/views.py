418464843c0a78a69b4c030406a83c2fefb69f68
suresh <suresh12_dmm@yahoo.com>
2020-03-31 13:25:00 +0100

    if uswr is not logged in redirect  back to login page

--- level_base_binary_options/account/views.py
+++ level_base_binary_options/account/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from utilities.authentication import Authentication
 
 # Create your views here.
 
 def account(request):
+    ac = Authentication(request)
+    # if user is not logged in redirect to login page
+    if not ac.is_user_logged_in():
+        return redirect('/login')
     return render(request, 'account.html')
 
