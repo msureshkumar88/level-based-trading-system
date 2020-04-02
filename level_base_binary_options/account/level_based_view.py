59b3753c49249e0e21abb84e07d527d5f093eafa
suresh <suresh12_dmm@yahoo.com>
2020-04-02 18:04:24 +0100

    level baed trading: add view, UI and route

---
+++ level_base_binary_options/account/level_based_view.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render, redirect
+from utilities.authentication import Authentication
+
+from django.db import connection
+from datetime import datetime
+from datetime import timedelta
+
+from utilities.helper import Helper
+from .models import UserTransactionsBinary
+from .models import TransactionsByStatusBinary
+
+
+def levels(request):
+    ac = Authentication(request)
+    # if user is not logged in redirect to login page
+    if not ac.is_user_logged_in():
+        return redirect('/login')
+    data = dict()
+    if request.method == "POST":
+        pass
+
+    return render(request, 'level_based.html', data)
--- level_base_binary_options/account/urls.py
+++ level_base_binary_options/account/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
 from . import views
+from . import level_based_view
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('account', views.account, name='account'),
+    path('account/levels', level_based_view.levels, name='levels'),
 ]
 
