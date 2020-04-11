901c03d228ff8d3229d7e4be6a7e224936361eed
suresh <suresh12_dmm@yahoo.com>
2020-04-11 18:40:41 +0100

    create a new db model for creating trades

--- level_base_binary_options/account/models.py
+++ level_base_binary_options/account/models.py
@@ -126,3 +126,91 @@ class UsersOwnedLevelsStatus(DjangoCassandraModel):
     amount = columns.DateTime()
     outcome = columns.Text()
 
+
+class TransactionsByUser(DjangoCassandraModel):
+    id = columns.UUID(primary_key=True, default=uuid.uuid4)
+    user_id = columns.UUID()
+    created_date = columns.DateTime()
+
+
+class UserTransactions(DjangoCassandraModel):
+    transaction_id = columns.UUID(primary_key=True)
+    user_id = columns.UUID()
+    created_date = columns.DateTime()
+    trade_type = columns.Text()
+    purchase_type = columns.Text()
+    currency = columns.Text()
+    staring_price = columns.Float()
+    closing_price = columns.Float()
+    amount = columns.Float()
+    start_time = columns.DateTime()
+    end_time = columns.DateTime()
+    changes_allowed_time = columns.DateTime()
+    outcome = columns.Text()
+    status = columns.Text()
+    time_close = columns.DateTime()
+    level_pips = columns.Integer()
+    levels_price = columns.Text()
+    level_owners = columns.Text()
+    join_date = columns.DateTime()
+    level_start_price = columns.Float()
+    level_end_price = columns.Float()
+    level_selected = columns.Integer()
+    created_by = columns.UUID()
+    parent_id = columns.UUID()
+    child = columns.Boolean()
+    available_levels = columns.List(columns.Integer())
+
+
+class TransactionsByState(DjangoCassandraModel):
+    transaction_id = columns.UUID()
+    user_id = columns.UUID()
+    currency = columns.Text()
+    purchase_type = columns.Text()
+    outcome = columns.Text()
+    status = columns.Text(primary_key=True)
+    created_date = columns.DateTime()
+    amount = columns.Float()
+    trade_type = columns.Text()
+
+
+class TransactionsLevelsStatus(DjangoCassandraModel):
+    transaction_id = columns.UUID()
+    user_id = columns.UUID()
+    outcome = columns.Text()
+    status = columns.Text(primary_key=True)
+    purchase_type = columns.Text()
+    currency = columns.Text()
+    created_date = columns.DateTime()
+    amount = columns.Float()
+    trade_type = columns.Text()
+    start_time = columns.DateTime()
+    end_time = columns.DateTime()
+    available_levels = columns.List(columns.Integer())
+
+
+class TransactionsByStartTime(DjangoCassandraModel):
+    transaction_id = columns.UUID()
+    user_id = columns.UUID()
+    status = columns.Text(primary_key=True)
+    start_time = columns.DateTime()
+
+
+class TransactionsByEndTime(DjangoCassandraModel):
+    transaction_id = columns.UUID()
+    user_id = columns.UUID()
+    status = columns.Text(primary_key=True)
+    end_time = columns.DateTime()
+    trade_type = columns.Text()
+
+
+class TransactionsChangesAllowedTime(DjangoCassandraModel):
+    transaction_id = columns.UUID()
+    user_id = columns.UUID()
+    status = columns.Text(primary_key=True)
+    changes_allowed_time = columns.DateTime()
+
+
+class LevelBasedUserCounts(DjangoCassandraModel):
+    transaction_id = columns.UUID(primary_key=True)
+    user_count = columns.Integer()
