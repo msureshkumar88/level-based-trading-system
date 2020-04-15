c7dbe303de4a8550c489b6f8180c2cb45e4c4042
suresh <suresh12_dmm@yahoo.com>
2020-04-15 11:39:56 +0100

    search level based trades: crrate filtering paraments

--- level_base_binary_options/account/search_level_based_view.py
+++ level_base_binary_options/account/search_level_based_view.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render, redirect
 from utilities.authentication import Authentication
 
-from utilities.purchase_type import PurchaseTypes
 from utilities.helper import Helper
 
 from django.db import connection
+
+from utilities.trade_status import Status
+
 
 def level_based_search(request):
     ac = Authentication(request)
@@ -16,10 +18,7 @@ def level_based_search(request):
     if request.method == "POST":
         search(request)
 
-    purchase_type = []
-    [purchase_type.append(e.value) for e in PurchaseTypes]
-
-    data['purchase_type'] = purchase_type
+    data['purchase_type'] = Helper.get_purchase_type_list()
     data['currency_pairs'] = Helper.get_currency_pairs()
 
     return render(request, 'level_trade_search.html', data)
@@ -42,6 +41,36 @@ def search(request):
     user_id = ac.get_user_session()
     cursor = connection.cursor()
 
+    initial_query = f"SELECT * FROM transactions_by_state WHERE status = {Status.STARTED.value} "
+
+
+def filter_where_currency_pair(currency_pair):
+    return f"AND currency = '{currency_pair}' "
+
+
+def filter_where_currency_pairs_in():
+    return f"AND currency in {tuple(Helper.get_currency_pairs_list())} "
+
+
+def filter_where_purchase_type(purchase_type):
+    return f"AND purchase_type = '{purchase_type}' "
+
+
+def filter_where_purchase_type_in():
+    return f"AND purchase_type in {tuple(Helper.get_purchase_type_list())} "
+
+
+# filter by trade closing time - get the results greater than minimum closing time
+def filter_where_closing_time(closing_time):
+    return f"AND end_time > '{closing_time}' "
+
+
+# filter amount range
+def filter_where_amount(min_amount, max_amount):
+    return f"AND amount > {min_amount} AND amount < {max_amount} "
+
+
+# TODO: filter when both amount and closing date submitted
 
 def search_all_inputs(request):
     for filed in request.POST:
