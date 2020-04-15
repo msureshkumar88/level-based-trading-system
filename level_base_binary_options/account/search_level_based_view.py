35119332873257856d60ce117d69e995600f567f
suresh <suresh12_dmm@yahoo.com>
2020-04-15 13:48:50 +0100

    level based search: implement partial search

--- level_base_binary_options/account/search_level_based_view.py
+++ level_base_binary_options/account/search_level_based_view.py
@@ -40,12 +40,35 @@ def search(request):
     ac = Authentication(request)
     user_id = ac.get_user_session()
     cursor = connection.cursor()
+    single_status = f"('{Status.STARTED.value}')"
+    initial_query = f"SELECT * FROM transactions_levels_status WHERE status in {single_status} "
 
-    initial_query = f"SELECT * FROM transactions_by_state WHERE status = {Status.STARTED.value} "
+    if currency:
+        initial_query = initial_query + filter_where_currency_pair(currency)
+
+    if not currency:
+        initial_query = initial_query + filter_where_currency_pairs_in()
+    # TODO: fix filters end_date before filtering purchase type
+    # if purchase_type:
+    #     initial_query = initial_query + filter_where_purchase_type(currency)
+    #
+    # if not purchase_type:
+    #     initial_query = initial_query + filter_where_purchase_type_in()
+
+    # TODO: extend  this search to work with trade closing date and amount range
+
+    print(initial_query)
+    results = cursor.execute(initial_query)
+    if not results:
+        return []
+
+    for r in results:
+        print(r)
 
 
 def filter_where_currency_pair(currency_pair):
-    return f"AND currency = '{currency_pair}' "
+    single_currency = f"('{currency_pair}')"
+    return f"AND currency in {single_currency} "
 
 
 def filter_where_currency_pairs_in():
@@ -53,7 +76,8 @@ def filter_where_currency_pairs_in():
 
 
 def filter_where_purchase_type(purchase_type):
-    return f"AND purchase_type = '{purchase_type}' "
+    single_purchase_type = f"('{purchase_type}')"
+    return f"AND purchase_type in {single_purchase_type} "
 
 
 def filter_where_purchase_type_in():
